c6209af2484c52a6fcbc9428d889818fa8b59800
jiamian2009 <48003443+jiamian2009@users.noreply.github.com>
2022-11-15 10:05:35 +0800

    Update build_onnx.bat

--- build_onnx.bat
+++ build_onnx.bat
@@ -1,3 +1,4 @@
 SET arch=--x86
 if %2==x64 (SET arch=)
-.\build.bat --config %1 %arch% --parallel --use_openmp --build_shared_lib --cmake_generator "Visual Studio 16 2019"
+% .\build.bat --config %1 %arch% --parallel --use_openmp --build_shared_lib --cmake_generator "Visual Studio 16 2019"
+.\build.bat --config %1 %arch% --parallel --use_openmp --build_shared_lib --cmake_generator "MinGW Makefiles"
